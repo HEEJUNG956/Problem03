ac989e89a8d1eaedc80fc3a05e42da4f94552f53
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-31 11:30:47 +0900

    나머지가1이되는숫자찾기

---
+++ 3페이지 level 1 04번 - 나머지가1이되는숫자찾기/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3페이지_level_1_04번___나머지가1이되는숫자찾기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                if(n % i == 1)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+            
+        }
+    }
+}
