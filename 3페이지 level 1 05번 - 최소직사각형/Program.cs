fc6aa406bf37cc148f050f6b59e29b76294a4270
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-31 12:04:04 +0900

    최소직사각형

---
+++ 3페이지 level 1 05번 - 최소직사각형/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3페이지_level_1_05번___최소직사각형
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] sizes = { { } };
+            List<int[]> list = new List<int[]>();
+
+            for (int i = 0; i < sizes.GetLength(0); i++)
+            {
+                int temp = 0;
+                if (sizes[i, 0] < sizes[i, 1])
+                {
+                    temp = sizes[i, 1];
+                    sizes[i, 1] = sizes[i, 0];
+                    sizes[i, 0] = temp;
+                }
+                list.Add(new int[2]{sizes[i, 0], sizes[i, 1]});
+            }
+            Console.WriteLine(list.Max(x => x[0]) * list.Max(x => x[1]));
+        }
+    }
+}
