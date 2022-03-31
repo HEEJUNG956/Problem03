d3c8ec0649273a01894c66eb290cf5f83cbd9ab8
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-31 10:47:16 +0900

    가운데글자가져오기

---
+++ 3페이지 level 1 02번 - 가운데글자가져오기/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3페이지_level_1_02번___가운데글자가져오기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string s = Console.ReadLine();
+            int a = (s.Length + 1) % 2;
+            int b = s.Length / 2 - a;
+
+            Console.WriteLine(s.Substring(b, a + 1));
+        }
+    }
+}
