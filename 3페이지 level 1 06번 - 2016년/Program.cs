c171ff02f72a9919c4d0d8eebe1a0ed7a4ab9723
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-31 13:58:16 +0900

    2016년

---
+++ 3페이지 level 1 06번 - 2016년/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3페이지_level_1_06번___2016년
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("월 : ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("일 : ");
+            int b = int.Parse(Console.ReadLine());
+
+            string answer = "";
+            DateTime dateValue = new DateTime(2016, a, b);
+            answer = dateValue.DayOfWeek.ToString();
+            Console.WriteLine(answer);
+            answer = answer.Substring(0, 3).ToUpper();
+            Console.WriteLine(answer);
+        }
+    }
+}
