44fc4928296cf6bd11d1b1f5d3f4af00f7331bb4
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-31 10:25:51 +0900

    나누어떨어지는배열

---
+++ 3페이지 level 1 01번 - 나누어떨어지는배열/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3페이지_level_1_01번___나누어떨어지는배열
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()) , int.Parse(Console.ReadLine()) };
+            int divisor = int.Parse(Console.ReadLine());
+            List<int> result = new List<int>();
+
+            foreach(var item in arr)
+            {
+                if(item % divisor == 0)
+                {
+                    result.Add(item);
+                }
+            }
+            if(result.Count == 0)
+            {
+                result.Add(-1);
+            }
+            else
+            {
+                result.Sort();
+            }
+            
+            for(int i = 0; i < result.Count; i++)
+            {
+                Console.Write(result[i] + " ");  
+            }
+        }
+    }
+}
