b67883a02e73fe4ef79d0256f8f9c5bc42019e70
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-31 15:42:57 +0900

    약수의개수

---
+++ 3페이지 level 1 10번 - 약수의개수와덧셈/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3페이지_level_1_10번___약수의개수와덧셈
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("왼쪽 숫자 입력하기 => ");
+            int left = int.Parse(Console.ReadLine());
+            Console.Write("오른쪽 숫자 입력하기 => ");
+            int right = int.Parse(Console.ReadLine());
+
+            int answer = 0;
+            int count = 0;
+            for(int i = left; i <= right; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0) count++;
+                }
+                answer += (count % 2 == 0) ? i : i * -1;
+                count = 0;
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
